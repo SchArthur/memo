946e83934f6e90594ec71804aab24fd658cd0938
Arthur <arthur.9996@gmail.com>
2025-12-02 14:47:31 +0100

    🎉 init : game file structure

---
+++ game.php
@@ -0,0 +1,24 @@
+<?php
+
+$game_width = 5; // Nombre de cartes par ligne
+$game_height = 5; // Nombre de lignes de cartes
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/game.css">
+    <title>Mémo</title>
+</head>
+
+<body>
+    <h1>Jeu de mémo !</h1>
+    <div id="game">
+
+    </div>
+</body>
+
+</html>
